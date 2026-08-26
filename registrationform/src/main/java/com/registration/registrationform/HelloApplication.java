a75b40b5546c2c00672b365daf8dac93cd8e05a0
Shashank K <sharathshashank1986@gmail.com>
2026-08-26 12:42:17 +0530

    registration form

---
+++ registrationform/src/main/java/com/registration/registrationform/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.registration.registrationform;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
