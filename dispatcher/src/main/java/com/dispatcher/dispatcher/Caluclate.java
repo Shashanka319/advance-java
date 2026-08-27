4847fc90965609e4831cb397137cc66bd5e9b95b
Shashank K <sharathshashank1986@gmail.com>
2026-08-27 11:42:59 +0530

    completed

--- dispatcher/src/main/java/com/dispatcher/dispatcher/Caluclate.java
+++ dispatcher/src/main/java/com/dispatcher/dispatcher/Caluclate.java
@@ -1,4 +1,29 @@
 package com.dispatcher.dispatcher;
 
-public class Caluclate {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/caluclate")
+public class Caluclate extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int num1 =Integer.parseInt(req.getParameter("FN"));
+        int num2 =Integer.parseInt(req.getParameter("SN"));
+
+        int sum =num1+num2;
+        int product =num1*num2;
+
+        req.setAttribute("mysum",sum);
+        req.setAttribute("myproduct",product);
+
+        RequestDispatcher rd= req.getRequestDispatcher("display");
+        rd.forward(req,resp);
+
+    }
 }
---
+++ dispatcher/src/main/java/com/dispatcher/dispatcher/Display.java
@@ -0,0 +1,22 @@
+package com.dispatcher.dispatcher;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/display")
+public class Display extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int sum =(int) req.getAttribute("mysum");
+        int product =(int) req.getAttribute("myproduct");
+
+        PrintWriter out = resp.getWriter();
+        out.println("Sum: "+sum);
+        out.println("Product: "+product);
+    }
+}
